e02d9540b4b21d38b2ed93e6d4f170b0891ce890
eduardogozlez <eduardo.gonzalez-valero@iesruizgijon.com>
2025-03-10 18:36:35 +0100

    Creación de componentes y configuración de rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BienvenidaComponent } from './components/bienvenida/bienvenida.component';
+import { BusquedaComponent } from './components/busqueda/busqueda.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { PaginaNoEncontradaComponent } from './components/pagina-no-encontrada/pagina-no-encontrada.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: BienvenidaComponent },
+  { path: 'busqueda', component: BusquedaComponent },
+  { path: 'acerca-de', component: AcercaDeComponent },
+  { path: '**', component: PaginaNoEncontradaComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienvenidaComponent } from './components/bienvenida/bienvenida.component';
+import { BusquedaComponent } from './components/busqueda/busqueda.component';
+import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { PaginaNoEncontradaComponent } from './components/pagina-no-encontrada/pagina-no-encontrada.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidaComponent,
+    BusquedaComponent,
+    AcercaDeComponent,
+    PaginaNoEncontradaComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
